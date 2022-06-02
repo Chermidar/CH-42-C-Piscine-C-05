bda1c0f272dbb12e71094be1580813a57515c500
Carlos Hermida ron <chermida@c2r9s2.42madrid.com>
2022-06-02 15:17:08 +0200

    missing ex08

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -12,24 +12,6 @@
 
 #include <stdio.h>
 
-int	ft_sqrt(int nb)
-{
-	int	aux;
-
-	aux = 0;
-	if (nb == 1)
-		return (1);
-	while (aux <= nb / 2)
-	{
-		if (aux > 46340)
-			return (0);
-		if (aux * aux == nb)
-			return (aux);
-		aux++;
-	}
-	return (0);
-}
-
 int	ft_is_prime(int nb)
 {
 	int	n;
@@ -37,9 +19,7 @@ int	ft_is_prime(int nb)
 	n = 2;
 	if (nb < 2)
 		return (0);
-	if (ft_sqrt(nb) != 0)
-		return (0);
-	while (n < nb / n)
+	while (n <= nb / n)
 	{
 		if (nb % n == 0)
 			return (0);
